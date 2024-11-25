53f9ba7bff4ac0bc81fb110737a60b7f86ec893a
Brendo Gomes <brensogs2021@gmail.com>
2024-11-25 18:04:43 -0300

    Aula 128 - Conhecendo as server action

--- src/app/dashboard/new/page.tsx
+++ src/app/dashboard/new/page.tsx
@@ -1,9 +1,11 @@
-import Container from '@/components/container';
 import Link from 'next/link';
-import { getServerSession } from 'next-auth';
-import { AuthOprions } from '@/lib/auth';
 import { redirect } from 'next/navigation';
+import { getServerSession } from 'next-auth';
+
+import Container from '@/components/container';
+import { AuthOprions } from '@/lib/auth';
 import prisma from '@/lib/prisma';
+
 export default async function NewTicket() {
 
     const session = await getServerSession(AuthOprions)
@@ -18,6 +20,20 @@ export default async function NewTicket() {
         }
     })
 
+    async function handleResgisterTicket(formData: FormData){
+        "use server"
+
+        const name = formData.get("name")
+        const description = formData.get("description")
+        const costumerId = formData.get("customer")
+
+
+        if(!name || !description || !costumerId){
+            return
+        }
+
+    }
+
     return (
         <Container>
             <main className='mt-9 mb-2'>
@@ -25,20 +41,24 @@ export default async function NewTicket() {
                     <Link href="/dashboard" className='text-white px-4 py-1 rounded bg-gray-900'>Voltar</Link>
                     <h1 className='text-3xl font-bold'>Novo Chamados</h1>
                 </div>
-                <form className='flex flex-col mt-6'>
+                <form className='flex flex-col mt-6' action={handleResgisterTicket}>
                     <label className='mb-1 font-medium text-lg'>Nome do Chamado</label>
                     <input
                         className='w-full border-2 rounded-md px-2 mb-2 h-11'
                         type="text"
+                        name='name'
                         placeholder='Digite o nome do chamado' required />
                     <label className='mb-1 font-medium text-lg'>Descreva o problema</label>
                     <textarea
+                        name='description'
                         className='w-full border-2 rounded-md px-2 mb-2 h-24 resize-none'
                         placeholder='Descreva o problema...' required></textarea>
                     {ListCostumer.length !== 0 && (
                         <>
                             <label className='mb-1 font-medium text-lg'>Selecione o cliente</label>
-                            <select className='w-full border-2 rounded-md px-2 mb-2 h-11 resize-none bg-white'>
+                            <select
+                                name='customer'
+                                className='w-full border-2 rounded-md px-2 mb-2 h-11 resize-none bg-white'>
                                 {ListCostumer.map((item) => {
                                     return (
                                         <option key={item.id} value={item.id}>{item.name}</option>
@@ -48,10 +68,10 @@ export default async function NewTicket() {
                         </>
                     )}
 
-                    <button 
-                    type='submit'
-                    className='bg-blue-500 text-white font-bold px-2 h-11 rounded-md my-4 disabled:bg-gray-400 disabled:cursor-not-allowed'
-                    disabled={ListCostumer.length === 0}>Cadastrar</button>
+                    <button
+                        type='submit'
+                        className='bg-blue-500 text-white font-bold px-2 h-11 rounded-md my-4 disabled:bg-gray-400 disabled:cursor-not-allowed'
+                        disabled={ListCostumer.length === 0}>Cadastrar</button>
 
                 </form>
             </main>
