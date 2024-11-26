5cef85dcfa54678d85cecb49ebcae7bf63cd480d
Brendo Gomes <brensogs2021@gmail.com>
2024-11-26 13:25:55 -0300

    Aula 138 - Formulário open ticket

--- src/app/open/page.tsx
+++ src/app/open/page.tsx
@@ -4,9 +4,12 @@ import { useState } from "react";
 import { z } from "zod"
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { FiSearch } from "react-icons/fi";
+import { FiSearch, FiX } from "react-icons/fi";
+
 
 import Input from "@/components/input"
+import FormTicket from "./components/FormTicket";
+
 const schema = z.object({
     email: z.string().email('Digite o email do cliente para localizar').min(1, 'Campo obrigatório')
 })
@@ -20,12 +23,16 @@ type FormData = z.infer<typeof schema>
 
 export default function OpenTicket() {
 
-    const [customer, setCustomer] = useState<CustomerDataInfor | null>(null);
+    const [customer, setCustomer] = useState<CustomerDataInfor | null>({ id: '1', name: 'João' });
 
     const { handleSubmit, register, setValue, formState: { errors } } = useForm<FormData>({
         resolver: zodResolver(schema)
     })
 
+    function handleClearCustomer() {
+        setCustomer(null);
+        setValue("email", "")
+    }
 
     return (
         <section className="w-full max-w-2xl mx-auto px-2 ">
@@ -33,8 +40,13 @@ export default function OpenTicket() {
 
             <article className="flex flex-col mt-4 mb-2">
                 {customer ? (
-                    <div>
-                        <h1>teste</h1>
+                    <div className="bg-slate-200 py-6 px-4 rounded border-2 flex items-center justify-between">
+                        <p className="text-lg">
+                            <strong>Cliente Selecionado: </strong> {customer.name}
+                        </p>
+                        <button className="h-11 px-2 items-center justify-center rounded" onClick={handleClearCustomer}>
+                            <FiX size={30} color="#ff2929" />
+                        </button>
                     </div>
                 ) : (
                     <form className="bg-slate-200 py-6 px-2 rounded border-2">
@@ -53,6 +65,10 @@ export default function OpenTicket() {
                         </div>
                     </form>
                 )}
+                {customer !== null && (
+                    <FormTicket />
+                )}
+
             </article>
 
         </section>
